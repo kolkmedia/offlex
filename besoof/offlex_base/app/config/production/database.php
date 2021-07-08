df056052daefa2acc2b8f8bf5c2392e5ffb2bb3b
Vincent van der Kolk <vincent@kolkmedia.com>
2021-07-08 16:10:59 +0200

    db besoof

--- besoof/offlex_base/app/config/production/database.php
+++ besoof/offlex_base/app/config/production/database.php
@@ -5,7 +5,7 @@ return array(
 		'mysql' => array(
 		'driver'    => 'mysql',
 		'host'      => '172.18.0.1',
-		'port'      => '33061',
+		'port'      => '33062',
 		'database'  => 'offlex_besoof',
 		'username'  => 'root',
 		'password'  => 'root',
